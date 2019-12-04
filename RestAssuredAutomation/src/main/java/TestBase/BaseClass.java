4b736e8af71a4f0c2e2870422d0c2257b8bfb4d2
Divy <Divy@192.168.1.4>
2019-12-04 18:18:44 +0530

    updated base class

--- RestAssuredAutomation/src/main/java/TestBase/BaseClass.java
+++ RestAssuredAutomation/src/main/java/TestBase/BaseClass.java
@@ -5,12 +5,8 @@ import org.apache.log4j.*;
 import org.apache.poi.poifs.property.PropertyConstants;
 import org.testng.annotations.BeforeClass;
 
-import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 
 public class BaseClass {
-	public static RequestSpecification httpRequest;
-	public static Response response;
 	public String empId="54768";
 	
 	public Logger logger;
