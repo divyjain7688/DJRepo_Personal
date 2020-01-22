758f8aec31e03821391ef61eccff2b0394a076b7
HDS\dj073227 <divy.jain@hdsupply.com>
2020-01-22 16:14:45 +0530

    update changes

--- RestAssuredAutomation/src/test/java/udemyApiTest/PostGoogleApiSerializationWithSpecBuilders.java
+++ RestAssuredAutomation/src/test/java/udemyApiTest/PostGoogleApiSerializationWithSpecBuilders.java
@@ -43,7 +43,6 @@ public class PostGoogleApiSerializationWithSpecBuilders {
 		addQueryParam("key","qaclick123").
 		setContentType(ContentType.JSON).build();	
 		ResponseSpecification resspec =new ResponseSpecBuilder().expectStatusCode(200).expectContentType(ContentType.JSON).build();
-		
 		RequestSpecification res=given().spec(req)
 		.body(a);
 		Response response =res.when().post("/maps/api/place/add/json").
