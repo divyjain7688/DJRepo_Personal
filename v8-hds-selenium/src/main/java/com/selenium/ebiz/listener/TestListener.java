5b6595cb940f79f4e0525f7568340b297fb51633
divy.jain <divy.jain@LP1-AP-51860169.PROD.HCLPNP.COM>
2021-12-28 10:33:30 +0530

    test commit

--- v8-hds-selenium/src/main/java/com/selenium/ebiz/listener/TestListener.java
+++ v8-hds-selenium/src/main/java/com/selenium/ebiz/listener/TestListener.java
@@ -18,6 +18,7 @@ public class TestListener extends BaseClassV8_Parallel implements ITestListener 
         return iTestResult.getName();
     }
 
+    
     @Override
     public void onStart(ITestContext iTestContext) {
         System.out.println("I am in onStart method " + iTestContext.getName());
